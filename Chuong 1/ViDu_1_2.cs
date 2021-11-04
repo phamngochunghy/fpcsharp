9f666690cd996c27f86a42d28eb7b424c03cef48
phamngochunghy <87796018+phamngochunghy@users.noreply.github.com>
2021-11-04 11:26:14 +0700

    Create ViDu_1_2.cs

---
+++ Chuong 1/ViDu_1_2.cs
@@ -0,0 +1,16 @@
+﻿//Ví dụ 1.2
+//Chương trình C# hiển thị ra màn hình dòng chữ Hello World
+using System;
+namespace Bai1
+{
+    class ViDu11
+    {
+        //Hàm chính
+        static void Main(string[] args)
+        {
+            //Hiển thị ra màn hình dòng chữ Hello Fit UteHY
+            Console.WriteLine("Hello Fit UteHY!");
+            Console.ReadKey();
+        }
+    }
+}
